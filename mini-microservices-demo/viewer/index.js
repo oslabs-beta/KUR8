e105a5da0aa643544ee8b09ddf8287f0f7682fd2
ivy118 <47249996+ivy118@users.noreply.github.com>
2021-07-13 21:14:50 -0400

    fv

--- mini-microservices-demo/viewer/index.js
+++ mini-microservices-demo/viewer/index.js
@@ -1,58 +1,56 @@
+const path = require('path');
 const express = require('express');
-
 const app = express();
-
+app.use(express.json());
 const k8s = require('@kubernetes/client-node');
-
 const kc = new k8s.KubeConfig();
 kc.loadFromDefault();
-
 const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
+// app.use(express.static(path.join(__dirname, '../infra/k8s')));
 
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
 
 
 app.get('/podList', (req, res) => {
-  k8sApi2.listNamespacedPod('default')
-  .then(res => {
-    return res.status(201).send(JSON.stringify(res.body, null, 2));
-  }).catch(err => {
-      res.send("error found in get request to /podList", err)
-  })
+  k8sApi
+    .listNamespacedPod('default')
+    .then(data => res.send(data.body))
+    .catch(err => {
+      res.send('error found in get request to /podList', err);
+    });
 });
-
 
 
 app.get('/serviceList', (req, res) => {
-  k8sApi2.listNamespacedService('default')
-  .then(res => {
-    return res.status(201).send(JSON.stringify(res.body, null, 2));
-  }).catch(err => {
-      res.send("error found in get request to /serviceList", err)
-  })
+  k8sApi
+    .listNamespacedService('default')
+    .then(data => res.send(data.body))
+    .catch(err => {
+      res.send('error found in get request to /serviceList', err);
+    });
 });
-
 
 
 app.get('/ingressList', (req, res) => {
-  k8sApi2.listNamespacedIngress('default')
-  .then(res => {
-    return res.status(201).send(JSON.stringify(res.body, null, 2));
-  }).catch(err => {
-      res.send("error found in get request to /ingressList", err)
-  })
+  k8sApi
+    .listNamespacedIngress('default')
+    .then(data => res.send(data.body))
+    .catch(err => {
+      res.send('error found in get request to /ingressList', err);
+    });
 });
-
 
 
 app.get('/deploymentList', (req, res) => {
-  k8sApi2.listNamespacedDeployment('default')
-  .then(res => {
-    return res.status(201).send(JSON.stringify(res.body, null, 2));
-  }).catch(err => {
-      res.send("error found in get request to /deploymentList", err)
-  })
+  k8sApi
+    .listNamespacedDeployment('default')
+    .then(data => res.send(data.body))
+    .catch(err => {
+      res.send('error found in get request to /deploymentList', err);
+    });
 });
-
 
 
 app.listen(5000, () => {
