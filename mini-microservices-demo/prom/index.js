fc8615e5f3493bc0d4fbb432e406c476c9c63b15
jimmycngo <jimmycngo@gmail.com>
2021-07-15 18:49:43 -0700

    got default prometheus metrics working

--- mini-microservices-demo/prom/index.js
+++ mini-microservices-demo/prom/index.js
@@ -1,5 +1,15 @@
-const client = require('prom-client');
-const collectDefaultMetrics = client.collectDefaultMetrics;
-const Registry = client.Registry;
-const register = new Registry();
-collectDefaultMetrics({ register });
+const express = require('express')
+const client = require('prom-client')
+
+client.collectDefaultMetrics()
+
+const app = express()
+app.get('/metrics', async (req, res) => {
+  console.log('Scraped')
+  console.log(await client.register.metrics())
+  res.send(await client.register.metrics())
+})
+
+app.listen(9991, () =>
+  console.log(`🚨 Prometheus listening on port 9991 /metrics`)
+)
