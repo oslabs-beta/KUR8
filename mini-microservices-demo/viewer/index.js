3f92e912afb55ab1412385fa0b8097044c73ed5d
ivy118 <47249996+ivy118@users.noreply.github.com>
2021-07-17 01:35:40 -0400

    trying to fix issue

--- mini-microservices-demo/viewer/index.js
+++ mini-microservices-demo/viewer/index.js
@@ -1,35 +1,29 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
+
 const k8s = require('@kubernetes/client-node');
 const kc = new k8s.KubeConfig();
 kc.loadFromDefault();
 const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
 const k8sApi2 = kc.makeApiClient(k8s.ExtensionsV1beta1Api);
-
-const getPodList = async (req, res, next) => {
-  const data = await k8s.topNodes(k8sApi);
-  // console.log(JSON.stringify(data));
-  res.locals.podList = data[0];
-  return next();
-  // k8sApi
-  //   .listNamespacedPod('default')
-  //   .then(data => {
-  //     res.locals.podList = data.body
-  //     return next()
-  //   })
-  //   .catch(err => {
-  //     res.status(500).send('error found in get request to /podList', err);
-  //   });
+const getPodList = (req, res, next) => {
+  k8sApi
+    .listNamespacedPod('default')
+    .then(data => {
+      res.locals.podList = data.body
+      return next()
+    })
+    .catch(err => {
+      res.status(500).send('error found in get request to /podList', err);
+    });
 };
+
 app.get('/podList', getPodList, (req, res) => {
   res.status(201).send(res.locals.podList);
 });
 
-
-console.log('jjj');
-
-
+console.log('jj');
 const getServiceList = (req, res, next) => {
   k8sApi
     .listNamespacedService('default')
@@ -44,11 +38,9 @@ const getServiceList = (req, res, next) => {
     });
 };
 
-
 app.get('/serviceList', getServiceList, (req, res) => {
   res.status(201).send(res.locals.serviceList);
 });
-
 
 const getIngressList = (req, res, next) => {
   k8sApi2
@@ -64,11 +56,9 @@ const getIngressList = (req, res, next) => {
     });
 };
 
-
 app.get('/ingressList', getIngressList, (req, res) => {
   res.status(201).send(res.locals.ingressList);
 });
-
 
 const getDeploymentList = (req, res, next) => {
   k8sApi
@@ -83,7 +73,6 @@ const getDeploymentList = (req, res, next) => {
         .send('error found in get request to /deploymentList', err);
     });
 };
-
 
 app.get('/deploymentList', getDeploymentList, (req, res) => {
   res.status(201).send(res.locals.deploymentList);
@@ -110,7 +99,6 @@ app.get('/nodeList', getNodeList, (req, res) => {
 app.get('/', (req, res) => {
   res.send('hello world');
 });
-
 
 app.listen(5000, () => {
   console.log('Listening on 5000');
