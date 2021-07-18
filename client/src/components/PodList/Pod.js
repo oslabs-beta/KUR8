ffad18770e79458f65e33b1ca0826ac723e62dd6
Steven Del Rosario <stevendelro@pm.me>
2021-07-18 13:11:05 -0500

    tentative changes to PodList and Pod views

--- client/src/components/PodList/Pod.js
+++ client/src/components/PodList/Pod.js
@@ -4,21 +4,29 @@ import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles(theme => ({
-  paper: {
-    height: theme.spacing(30),
-    width: theme.spacing(30),
+  outer: {
+    background: theme.palette.secondary
+  },
+  kubernetesShape: {
+    height: theme.spacing(20),
+    width: theme.spacing(20),
+    margin: theme.spacing(1),
+    borderRadius: '3px',
+    clipPath:
+      'polygon(50% 0%, 90% 20%, 100% 60%, 75% 100%, 25% 100%, 0% 60%, 10% 20%)',
   },
 }));
 
 function Pod({ containers, metadata, spec, status, podIndex }) {
   const classes = useStyles();
-  console.log(`status`, status);
   return (
-    <Paper className={classes.paper} elevation={3}>
-      <Typography as="body1">Name: {containers.list[0].name}</Typography>
-      <Typography as="body1">podIP: {status.podIP}</Typography>
-      <Typography as="body1">hostIP: {status.hostIP}</Typography>
-    </Paper>
+    <div className={classes.outer}>
+      <div className={classes.kubernetesShape} elevation={3}>
+        <Typography as="body1">Name: {containers.list[0].name}</Typography>
+        <Typography as="body1">podIP: {status.podIP}</Typography>
+        <Typography as="body1">hostIP: {status.hostIP}</Typography>
+      </div>
+    </div>
   );
 }
 
--- client/src/components/PodList/index.js
+++ client/src/components/PodList/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 
 import Pod from './Pod';
 
+const useStyles = makeStyles(theme => ({
+  paper: {
+    padding: theme.spacing(5),
+  },
+}));
+
 const PodList = ({ pods }) => {
+  const classes = useStyles();
   return (
-    <Paper elevation={1}>
+    <Paper className={classes.paper} elevation={1}>
+      <Typography>Node</Typography>
       <Grid
         container
         direction="row"
