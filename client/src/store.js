df68721ec89a95a6dc76be3f1a39a7f5dd4745c2
Steven Del Rosario <stevendelro@pm.me>
2021-07-21 12:09:08 -0500

    added comments

--- client/src/store.js
+++ client/src/store.js
@@ -18,7 +18,7 @@ const rootReducer = combineReducers({
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   rootReducer,
-  composeEnhancers(applyMiddleware(thunk))
+  composeEnhancers(applyMiddleware(thunk)) // Apply redux-thunk middleware. This gives passes the `dispatch` function as an argument to returned functions.
 );
 
 export default store;
