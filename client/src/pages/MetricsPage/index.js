7cf855ca7f808f06cb4895116fb7f600f6b51641
ivy118 <47249996+ivy118@users.noreply.github.com>
2021-07-20 23:32:16 -0400

    cpu and memory gauge

--- client/src/pages/MetricsPage/index.js
+++ client/src/pages/MetricsPage/index.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from 'react';
+import {CPUGauge} from './Components/cpu.jsx';
+import {MemoryGauge} from './Components/memory.jsx'
 
 function MetricsPage() {
   return (
     <div>
       metrics page
+      <CPUGauge />
+      <MemoryGauge />
     </div>
   )
 }
 
-export default MetricsPage
+export default MetricsPage;
