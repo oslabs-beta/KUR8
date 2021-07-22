1cbb1421f370fddf077b8d7ccb84fd619f3e7313
Steven Del Rosario <stevendelro@pm.me>
2021-07-21 20:21:12 -0500

    created Ingress component shape

--- client/src/components/PodList/index.js
+++ client/src/components/PodList/index.js
@@ -6,26 +6,44 @@ import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 
 import Pod from './Pod';
+import Ingress from './Ingress'
 
 const useStyles = makeStyles(theme => ({
   paper: {
     padding: theme.spacing(5),
   },
+  ingressContainer: {
+    cursor: 'pointer',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: theme.spacing(5, 0),
+    height: '50px',
+    width: '200px',
+    borderRadius: '25px',
+    boxShadow: theme.shadows[3],
+  },
 }));
 
-const PodList = ({ pods, services }) => {
+const PodList = ({ pods, services, ingresses }) => {
   const classes = useStyles();
   return (
     <Paper className={classes.paper} elevation={1}>
       <Typography>Node</Typography>
-
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center">
+        <Ingress ingresses={ingresses} />
+      </Grid>
       <Grid
         container
         direction="row"
         justifyContent="center"
         alignItems="center">
         {pods.map((pod, index) => {
-          console.log(services[index]);
           return (
             <Pod
               key={`pod-${index}`}
@@ -43,6 +61,7 @@ export default connect(
   state => ({
     pods: state.podsReducer.pods,
     services: state.servicesReducer.services,
+    ingresses: state.ingressesReducer.ingresses
   }),
   null
 )(PodList);
