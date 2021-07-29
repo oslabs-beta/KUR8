417d41e7d1183cc2622dc8b12598b162b55a45d7
ivy118 <ivkookie9@gmail.com>
2021-07-29 10:19:54 -0400

    pod by namespace done

--- client/src/components/Charts/PodByNamespace.js
+++ client/src/components/Charts/PodByNamespace.js
@@ -1,6 +1,105 @@
 //sum by (namespace) (kube_pod_info)
 
+import React, { Component } from 'react';
+import 'zingchart/es6';
+import ZingChart from 'zingchart-react';
+import { connect } from 'react-redux';
 
-//http://localhost:9090/api/v1/query_range?query=sum%20by%20(namespace)%20(kube_pod_info)&start=2021-07-28T01:53:02.662Z&end=2021-07-29T01:53:26.813Z&step=1m
+// EXPLICITLY IMPORT MODULE from node_modules
+import 'zingchart/modules-es6/zingchart-maps.min.js';
+import 'zingchart/modules-es6/zingchart-maps-usa.min.js';
 
-//`http://localhost:9090/api/v1/query_range?query=sum%20by%20(namespace)%20(kube_pod_info)&start=${new Date(new Date().setDate(new Date().getDate()-1)).toISOString()}&end=${new Date().toISOString()}&step=1m`
+//http://localhost:9090/api/v1/query?query=sum%20by%20(namespace)%20(kube_pod_info)&time=2021-07-28T01:53:02.662Z
+
+export class PodByNamespace extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        config: {
+            type: 'bar',
+            "title": {
+                "text": "Number of Pods by Namespace",
+                "font-color": "black",
+                "backgroundColor": "none",
+                "font-size": "22px",
+            },
+            plot: {
+              'border-radius': "9px", /* Rounded Corners */
+            },
+            'scale-x': {
+                label: { /* Scale Title */
+                    text: "Namespace",
+                },
+                labels: this.stateLabel(), /* Scale Labels */
+            },
+            'scale-y': {
+                label: { /* Scale Title */
+                    text: "Number of Pods",
+                },
+            },
+            "plot": {
+                "bars-space-left": 0.15,
+                "bars-space-right": 0.15,
+                "animation": {
+                  "effect": "ANIMATION_SLIDE_BOTTOM",
+                  "sequence": 0,
+                  "speed": 800,
+                  "delay": 800
+                }
+              },
+              "crosshair-x": {
+                "line-width": "100%",
+                "alpha": 0.18,
+                "plot-label": {
+                  "header-text": "%kv Pod"
+                }
+              },
+            series: [{
+                values: this.seriesFormat(),
+                'background-color': "#6666FF #FF0066", /* Bar fill color (gradient) */
+                "borderRadiusTopLeft": 7,
+                alpha: 0.3, /* Transparency (more transparent) */
+              },
+            ]
+        }
+      }
+      this.chartDone = this.chartDone.bind(this);
+    }
+
+    seriesFormat = () => {
+        //[[kubesystem, 9], ...]
+        let valueArr = [];
+
+        for (let i = 0; i < this.props.podPerNamespace.length; i++) {
+            valueArr.push(Number(this.props.podPerNamespace[i][1]));
+        }
+        return valueArr;
+    }
+
+    stateLabel = () => {
+        let labelArr = [];
+        for (let i = 0; i < this.props.podPerNamespace.length; i++) {
+            labelArr.push(this.props.podPerNamespace[i][0]);
+        }
+        return valueArr;
+
+    }
+    render() {
+
+        return (
+        <div>
+            <ZingChart data={this.state.config} complete={this.chartDone} />
+        </div>
+        );
+    }
+    chartDone(event) {
+        console.log(`Event "Complete" - The chart is rendered\n`);
+    }
+}
+
+    export default connect(
+    state => ({
+        podPerNamespace: state.metricsReducer.podPerNamespace,
+    }),
+    null
+    )(PodByNamespace);
