cf4485c31a79786d9ae9529e13b73c1201e54813
Steven Del Rosario <stevendelro@pm.me>
2021-07-29 21:26:45 -0500

    refactored PodList to render correct ClusterIP

--- client/src/components/PodList/index.js
+++ client/src/components/PodList/index.js
@@ -5,24 +5,30 @@ import Pod from './Pod';
 
 const PodList = ({ pods, services }) => {
   const clusterIPs = {};
-  const podNames = pods.map(pod => pod.containers.list[0].name);
+  const containerNames = [];
+
+  // Create an array of containerNames
+  pods.forEach(({ containers }) => {
+    Object.keys(containers[0]).forEach(name => containerNames.push(name));
+  });
 
   // Match ClusterIp's with the correct podName
-  services.forEach(service => {
+  services.forEach((service) => {
     const str = service.metadata.name;
     const nameWithoutSrv = str.substring(0, str.length - 4);
 
-    if (podNames.includes(nameWithoutSrv)) {
+    if (containerNames.includes(nameWithoutSrv)) {
       // Create the clusterIPs object with podNames as a key, and the clusterIP as the value
       clusterIPs[nameWithoutSrv] = service.spec.clusterIP;
     }
   });
+
   return (
     <Grid container direction="row" justifyContent="center" alignItems="center">
       {pods.map((pod, index) => (
         <Pod
           key={`pod-${index}`}
-          clusterIP={clusterIPs[pod.containers.list[0].name]}
+          clusterIP={clusterIPs[containerNames[index]]}
           {...pod}
         />
       ))}
