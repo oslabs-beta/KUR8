07d770d70efea86c32f0e406acf04c6cc3952bba
jimmycngo <jimmycngo@gmail.com>
2021-08-03 14:09:20 -0700

    linted alerts and metrics action cerators

--- client/src/actions/alertsActionCreator.js
+++ client/src/actions/alertsActionCreator.js
@@ -1,14 +1,15 @@
 import axios from 'axios';
 import * as actionTypes from './actionsTypes';
 
-export const receiveAlerts = data => {
+export const receiveAlerts = (data) => {
   return {
     type: actionTypes.RECEIVE_ALERTS,
     payload: data,
-  }
-}
+  };
+};
 
-export const fetchAlerts = () => dispatch => {
-  axios.get(`http://localhost:9090/api/v1/rules`)
-  .then(data => dispatch(receiveAlerts(data)));
-}
+export const fetchAlerts = () => (dispatch) => {
+  axios
+    .get(`http://localhost:9090/api/v1/rules`)
+    .then((data) => dispatch(receiveAlerts(data)));
+};
--- client/src/actions/metricsActionCreators.js
+++ client/src/actions/metricsActionCreators.js
@@ -1,113 +1,110 @@
 import axios from 'axios';
 import * as actionTypes from './actionsTypes';
 
-export const receiveDefaultMetrics = data => {
+export const receiveDefaultMetrics = (data) => {
   return {
     type: actionTypes.RECEIVE_DEFAULT_METRICS,
     payload: data,
   };
 };
 
-export const receiveQuery = data => {
+export const receiveQuery = (data) => {
   return {
     type: actionTypes.RECEIVE_QUERY,
     payload: data,
   };
 };
 
-export const receiveQueryRange = data => {
+export const receiveQueryRange = (data) => {
   return {
     type: actionTypes.RECEIVE_QUERY_RANGE,
     payload: data,
   };
 };
 
-export const receiveCpuQueryRange = data => {
+export const receiveCpuQueryRange = (data) => {
   return {
     type: actionTypes.RECEIVE_CPU_QUERY_RANGE,
     payload: data,
   };
 };
 
-export const fetchCPUNode = data => {
+export const fetchCPUNode = (data) => {
   return {
     type: actionTypes.FETCH_CPU_NODE,
     payload: data,
   };
 };
 
-
-
-export const fetchMemoryNode = data => {
-    return {
+export const fetchMemoryNode = (data) => {
+  return {
     type: actionTypes.FETCH_MEMORY_NODE,
     payload: data,
   };
 };
-  
-export const customQuery = data => {
+
+export const customQuery = (data) => {
   return {
     type: actionTypes.CUSTOM_QUERY,
     payload: data,
   };
 };
 
-export const fetchHTTPRequest = data => {
+export const fetchHTTPRequest = (data) => {
   return {
     type: actionTypes.FETCH_HTTP_REQUEST,
     payload: data,
   };
 };
 
-export const fetchCPUContainer = data => {
+export const fetchCPUContainer = (data) => {
   return {
     type: actionTypes.FETCH_CPU_CONTAINER,
     payload: data,
   };
 };
 
-export const fetchAllQueries = data => {
+export const fetchAllQueries = (data) => {
   return {
     type: actionTypes.ALL_PROMQL,
     payload: data,
-  }
-}
+  };
+};
 
-export const hyrateCustom = data => {
+export const hyrateCustom = (data) => {
   return {
     type: actionTypes.HYDRATE_CUSTOM,
     payload: data,
-  }
-}
+  };
+};
 
-export const deleteCustom = data => {
+export const deleteCustom = (data) => {
   return {
     type: actionTypes.DELETE_CUSTOM,
     payload: data,
-  }
-}
+  };
+};
 
-export const moveDnd = data => {
+export const moveDnd = (data) => {
   return {
     type: actionTypes.MOVE_DND,
     payload: data,
-  }
-}
+  };
+};
 
-export const numOfPodPerNamespace = data => {
+export const numOfPodPerNamespace = (data) => {
   return {
     type: actionTypes.FETCH_NUM_POD_NAMESPACE,
     payload: data,
-  }
-}
+  };
+};
 
-
-export const numOfPodNotReady = data => {
+export const numOfPodNotReady = (data) => {
   return {
     type: actionTypes.FETCH_POD_NOT_READY,
     payload: data,
-  }
-}
+  };
+};
 
 const timeFormat = (num) => {
   const now = Date.now();
@@ -117,9 +114,7 @@ const timeFormat = (num) => {
   const newTime = new Date(someHoursInTheFuture).toISOString();
 
   return newTime;
-
-}
-
+};
 
 const metricsActionCreators = [
   receiveDefaultMetrics, //using this one for garbage collection graph
@@ -131,7 +126,7 @@ const metricsActionCreators = [
   fetchHTTPRequest,
   fetchCPUContainer,
   numOfPodPerNamespace,
-  numOfPodNotReady
+  numOfPodNotReady,
 ];
 
 export const metricsEndpointArray = (query, start, end, step) => [
@@ -139,47 +134,64 @@ export const metricsEndpointArray = (query, start, end, step) => [
 
   // `http://localhost:9090/api/v1/query?query=rate(node_network_receive_bytes_total[1m])`
 
-   `http://localhost:9090/api/v1/query_range?query=sum by (cpu) (rate(node_cpu_seconds_total{mode="system"}[1m]))&start=${timeFormat(4)}&end=${new Date().toISOString()}&step=5m`,
-   
-  `http://localhost:9090/api/v1/query_range?query=rate(node_network_receive_bytes_total[1m])&start=${new Date(new Date().setDate(new Date().getDate()-1)).toISOString()}&end=${new Date().toISOString()}&step=5m`,
+  `http://localhost:9090/api/v1/query_range?query=sum by (cpu) (rate(node_cpu_seconds_total{mode="system"}[1m]))&start=${timeFormat(
+    4
+  )}&end=${new Date().toISOString()}&step=5m`,
+
+  `http://localhost:9090/api/v1/query_range?query=rate(node_network_receive_bytes_total[1m])&start=${new Date(
+    new Date().setDate(new Date().getDate() - 1)
+  ).toISOString()}&end=${new Date().toISOString()}&step=5m`,
 
   `http://localhost:9090/api/v1/query?query=100%20-%20(avg%20by%20(instance)%20(rate(node_cpu_seconds_total[1m]))%20*%20100)`,
 
-  `http://localhost:9090/api/v1/query_range?query=sum(container_memory_usage_bytes)%20by%20(node)&start=${new Date(new Date().setDate(new Date().getDate()-1)).toISOString()}&end=${new Date().toISOString()}&step=1m`,
+  `http://localhost:9090/api/v1/query_range?query=sum(container_memory_usage_bytes)%20by%20(node)&start=${new Date(
+    new Date().setDate(new Date().getDate() - 1)
+  ).toISOString()}&end=${new Date().toISOString()}&step=1m`,
 
-  `http://localhost:9090/api/v1/query_range?query=kubelet_http_requests_total&start=${new Date(new Date().setDate(new Date().getDate()-1)).toISOString()}&end=${new Date().toISOString()}&step=2m`,
+  `http://localhost:9090/api/v1/query_range?query=kubelet_http_requests_total&start=${new Date(
+    new Date().setDate(new Date().getDate() - 1)
+  ).toISOString()}&end=${new Date().toISOString()}&step=2m`,
 
   // `http://localhost:9090/api/v1/query_range?query=kubelet_http_requests_total&start=2021-07-27T04:04:26.785Z&end=2021-07-28T04:05:44.691Z&step=1m`,
 
   // `http://localhost:9090/api/v1/query_range?query=topk(5,%20rate(container_cpu_usage_seconds_total[5m]))&start=${new Date(new Date().setDate(new Date().getDate()-1)).toISOString()}&end=${new Date().toISOString()}&step=1m`,
-  `http://localhost:9090/api/v1/query_range?query=topk(5,%20rate(container_cpu_usage_seconds_total[5m]))&start=${timeFormat(4)}&end=${new Date().toISOString()}&step=30s`,
+  `http://localhost:9090/api/v1/query_range?query=topk(5,%20rate(container_cpu_usage_seconds_total[5m]))&start=${timeFormat(
+    4
+  )}&end=${new Date().toISOString()}&step=30s`,
 
   // `http://localhost:9090/api/v1/query_range?query=sum%20by%20(namespace)%20(kube_pod_info)&start=${new Date(new Date().setDate(new Date().getDate()-1)).toISOString()}&end=${new Date().toISOString()}&step=24h`,
 
   `http://localhost:9090/api/v1/query?query=sum%20by%20(namespace)%20(kube_pod_info)&time=${new Date().toISOString()}`,
 
-  `http://localhost:9090/api/v1/query_range?query=sum%20by%20(namespace)%20(kube_pod_status_ready{condition=%22false%22})&start=${new Date(new Date().setDate(new Date().getDate()-1)).toISOString()}&end=${new Date().toISOString()}&step=5m`,
-
+  `http://localhost:9090/api/v1/query_range?query=sum%20by%20(namespace)%20(kube_pod_status_ready{condition=%22false%22})&start=${new Date(
+    new Date().setDate(new Date().getDate() - 1)
+  ).toISOString()}&end=${new Date().toISOString()}&step=5m`,
 ];
 
 //on page load, call to metricsFetchdata, take url on line 31, create promises with values being map to the link, (data from each link); resolve promises and dispath action creator on line 25 to the corresopnding result in the URLl; which send it over to the reducers
-export const metricsFetchData = () => dispatch => {
+export const metricsFetchData = () => (dispatch) => {
   const urls = metricsEndpointArray();
-  const promises = urls.map(url => axios.get(url));
-  Promise.all(promises).then(values => {
+  const promises = urls.map((url) => axios.get(url));
+  Promise.all(promises).then((values) => {
     metricsActionCreators.forEach((actionCreator, index) => {
       dispatch(actionCreator(values[index]));
     });
   });
 };
 
-export const fetchCustomQuery = (query, range, step, title) => dispatch => {
-  console.log('fetching',query, range, step)
-  axios.get(`http://localhost:9090/api/v1/query_range?query=${query}&start=${new Date(new Date().setDate(new Date().getDate()-(range/24))).toISOString()}&end=${new Date().toISOString()}&step=${step}s`)
-  .then(data => dispatch(customQuery(data, title)));
-}
+export const fetchCustomQuery = (query, range, step, title) => (dispatch) => {
+  console.log('fetching', query, range, step);
+  axios
+    .get(
+      `http://localhost:9090/api/v1/query_range?query=${query}&start=${new Date(
+        new Date().setDate(new Date().getDate() - range / 24)
+      ).toISOString()}&end=${new Date().toISOString()}&step=${step}s`
+    )
+    .then((data) => dispatch(customQuery(data, title)));
+};
 
-export const fetchProm = () => dispatch => {
-  axios.get(`http://localhost:9090/api/v1/label/__name__/values`)
-  .then(data => dispatch(fetchAllQueries(data)));
-}
+export const fetchProm = () => (dispatch) => {
+  axios
+    .get(`http://localhost:9090/api/v1/label/__name__/values`)
+    .then((data) => dispatch(fetchAllQueries(data)));
+};
