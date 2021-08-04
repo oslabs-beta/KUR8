f6b751ac895d647a321041d1c3996703201ee7ab
Steven Del Rosario <stevendelro@pm.me>
2021-08-04 00:21:59 -0500

    cleanup charts. removed redux connect function

--- client/src/components/Charts/CPUGauge.js
+++ client/src/components/Charts/CPUGauge.js
@@ -1,41 +1,31 @@
-import React, { Component } from 'react';
-// import 'zingchart/es6';
-import ZingChart from 'zingchart-react';
-import { connect } from 'react-redux';
-import { makeStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
+import { withTheme } from '@material-ui/core/styles';
+import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
-import FormHelperText from '@material-ui/core/FormHelperText';
-import FormControl from '@material-ui/core/FormControl';
+import React, { Component } from 'react';
 import Select from '@material-ui/core/Select';
-import {withStyles} from "@material-ui/core/styles";
-
-// EXPLICITLY IMPORT MODULE from node_modules
-// import 'zingchart/modules-es6/zingchart-maps.min.js';
-// import 'zingchart/modules-es6/zingchart-maps-usa.min.js';
-
+import ZingChart from 'zingchart-react';
 
 const styles = theme => ({
   formControl: {
     margin: theme.spacing(1),
     minWidth: 140,
-    display: "flex",
+    display: 'flex',
   },
   selectEmpty: {
     marginTop: theme.spacing(2),
   },
 });
 
-
 export class CPUGauge extends Component {
   constructor(props) {
     super(props);
-    console.log('this.props.cpuGauge',this.props.cpuGauge)
     this.state = {
       config: {
         type: 'gauge',
-        "globals": {
-          "font-family": "Roboto"
+        globals: {
+          'font-family': 'Roboto',
         },
         title: {
           text: 'CPU usage in %',
@@ -46,56 +36,57 @@ export class CPUGauge extends Component {
         series: [
           {
             values: [Number(this.props.cpuGauge[0][2])],
-            "csize": "10%", //Needle Indicator Width
-            "size": "70%", //Needle Indicator Length
-            "background-color": "#66CCFF #FFCCFF"
+            csize: '10%', //Needle Indicator Width
+            size: '70%', //Needle Indicator Length
+            'background-color': '#66CCFF #FFCCFF',
           },
         ],
-          'scale-r': {
-            aperture: 270,     //Specify your scale range.
-            values: "0:100:20", //Provide min/max/step scale values.
-            center: {  //Pivot Point
-              type: "gear9",   //Specify your marker shape.
-              size:15,
-            },
-            ring: {  //Gauge Ring
-              size:10,
-              rules: [
-                {
-                  rule: "%v >= 0 && %v <= 20",
-                  'background-color': "#9EC1CF"
-                },
-                {
-                  rule: "%v >= 20 && %v <= 40",
-                  'background-color': "#9EE09E"
-                },
-                {
-                  rule: "%v >= 40 && %v <= 60",
-                  'background-color': "#FDFD97"
-                },
-                {
-                  rule: "%v >= 60 && %v <= 80",
-                  'background-color': "#FEB144"
-                },
-                {
-                  rule: "%v >= 80 && %v <= 100",
-                  'background-color': "#FF6663"
-                },
-              ]
-            }
-          }
+        'scale-r': {
+          aperture: 270, //Specify your scale range.
+          values: '0:100:20', //Provide min/max/step scale values.
+          center: {
+            //Pivot Point
+            type: 'gear9', //Specify your marker shape.
+            size: 15,
+          },
+          ring: {
+            //Gauge Ring
+            size: 10,
+            rules: [
+              {
+                rule: '%v >= 0 && %v <= 20',
+                'background-color': '#9EC1CF',
+              },
+              {
+                rule: '%v >= 20 && %v <= 40',
+                'background-color': '#9EE09E',
+              },
+              {
+                rule: '%v >= 40 && %v <= 60',
+                'background-color': '#FDFD97',
+              },
+              {
+                rule: '%v >= 60 && %v <= 80',
+                'background-color': '#FEB144',
+              },
+              {
+                rule: '%v >= 80 && %v <= 100',
+                'background-color': '#FF6663',
+              },
+            ],
+          },
+        },
       },
     };
     this.chartDone = this.chartDone.bind(this);
-    // console.log('this.props.cpuGauge', this.props.cpuGauge); 
-
+    // console.log('this.props.cpuGauge', this.props.cpuGauge);
   }
 
-  updateGauge = (nodeID) => {
+  updateGauge = nodeID => {
     let nodeData;
     this.props.cpuGauge.forEach(node => {
-      if (node[1] === nodeID) nodeData = node[2]
-    })
+      if (node[1] === nodeID) nodeData = node[2];
+    });
 
     this.setState({
       config: {
@@ -109,47 +100,49 @@ export class CPUGauge extends Component {
         series: [
           {
             values: [Number(nodeData)],
-            "csize": "10%", //Needle Indicator Width
-            "size": "70%", //Needle Indicator Length
-            "background-color": "#66CCFF #FFCCFF"
+            csize: '10%', //Needle Indicator Width
+            size: '70%', //Needle Indicator Length
+            'background-color': '#66CCFF #FFCCFF',
           },
         ],
-          'scale-r': {
-            aperture: 270,     //Specify your scale range.
-            values: "0:100:20", //Provide min/max/step scale values.
-            center: {  //Pivot Point
-              type: "gear9",   //Specify your marker shape.
-              size:15,
-            },
-            ring: {  //Gauge Ring
-              size:10,
-              rules: [
-                {
-                  rule: "%v >= 0 && %v <= 20",
-                  'background-color': "#9EC1CF"
-                },
-                {
-                  rule: "%v >= 20 && %v <= 40",
-                  'background-color': "#9EE09E"
-                },
-                {
-                  rule: "%v >= 40 && %v <= 60",
-                  'background-color': "#FDFD97"
-                },
-                {
-                  rule: "%v >= 60 && %v <= 80",
-                  'background-color': "#FEB144"
-                },
-                {
-                  rule: "%v >= 80 && %v <= 100",
-                  'background-color': "#FF6663"
-                },
-              ]
-            }
-          }
+        'scale-r': {
+          aperture: 270, //Specify your scale range.
+          values: '0:100:20', //Provide min/max/step scale values.
+          center: {
+            //Pivot Point
+            type: 'gear9', //Specify your marker shape.
+            size: 15,
+          },
+          ring: {
+            //Gauge Ring
+            size: 10,
+            rules: [
+              {
+                rule: '%v >= 0 && %v <= 20',
+                'background-color': '#9EC1CF',
+              },
+              {
+                rule: '%v >= 20 && %v <= 40',
+                'background-color': '#9EE09E',
+              },
+              {
+                rule: '%v >= 40 && %v <= 60',
+                'background-color': '#FDFD97',
+              },
+              {
+                rule: '%v >= 60 && %v <= 80',
+                'background-color': '#FEB144',
+              },
+              {
+                rule: '%v >= 80 && %v <= 100',
+                'background-color': '#FF6663',
+              },
+            ],
+          },
+        },
       },
     });
-  }
+  };
 
   //   [[kind-control-plane, Node 1, 87], [worker-node, Node 2, 109], [worker-node, Node 3, 71]]
   render() {
@@ -157,26 +150,26 @@ export class CPUGauge extends Component {
 
     return (
       <div>
-          <div>
-            {/* <select value={event.target.value} onChange={e => this.updateGauge(e.target.value)}>
+        <div>
+          {/* <select value={event.target.value} onChange={e => this.updateGauge(e.target.value)}>
             <option disabled>Select Node</option> */}
-              <FormControl variant="outlined" className={classes.formControl}>
-              <InputLabel id="demo-simple-select-outlined-label">Select Node</InputLabel>
-              <Select
-                labelId="demo-simple-select-outlined-label"
-                id="demo-simple-select-outlined"
-                value={event.target.value}
-                defaultValue=""
-                onChange={e => this.updateGauge(e.target.value)}
-                label="Select Node"
-              >
-                {this.props.cpuGauge.map((node, index) => {
-                return (
-                <MenuItem value={node[1]}>{node[0]}</MenuItem>
-                )})}
-              </Select>
-              </FormControl>
-            {/* {this.props.cpuGauge.map((node, index) => {
+          <FormControl variant="outlined" className={classes.formControl}>
+            <InputLabel id="demo-simple-select-outlined-label">
+              Select Node
+            </InputLabel>
+            <Select
+              labelId="demo-simple-select-outlined-label"
+              id="demo-simple-select-outlined"
+              value={event.target.value}
+              defaultValue=""
+              onChange={e => this.updateGauge(e.target.value)}
+              label="Select Node">
+              {this.props.cpuGauge.map((node, index) => {
+                return <MenuItem key={`menu-item-${index}`} value={node[1]}>{node[0]}</MenuItem>;
+              })}
+            </Select>
+          </FormControl>
+          {/* {this.props.cpuGauge.map((node, index) => {
               return <option key={`note-options-${index}`} value={node[1]}>{node[0]}</option>;
             })} */}
           {/* </select> */}
@@ -193,14 +186,4 @@ export class CPUGauge extends Component {
   }
 }
 
-
-
-// export connect(
-//   state => ({
-//     cpuGauge: state.metricsReducer.cpuGauge,
-//   }),
-//   null
-// )(CPUGauge);
-
-export default withStyles(styles, { withTheme: true })(CPUGauge);
-
+export default withStyles(styles)(withTheme(CPUGauge));
--- client/src/components/Charts/QueryCpuRangeChart.js
+++ client/src/components/Charts/QueryCpuRangeChart.js
@@ -1,115 +1,110 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-// import 'zingchart/es6';
 import ZingChart from 'zingchart-react';
-// EXPLICITLY IMPORT MODULE from node_modules
-// import 'zingchart/modules-es6/zingchart-maps.min.js';
-// import 'zingchart/modules-es6/zingchart-maps-usa.min.js';
 
 class QueryCpuRangeChart extends Component {
   constructor(props) {
     super(props);
-    console.log('this.props.cpuRangeChart',this.props.cpuRangeChart)
     this.dataFormat();
     this.state = {
       config: {
-        "globals": {
-          "font-family": "Roboto"
+        globals: {
+          'font-family': 'Roboto',
         },
-        "graphset": [{
-          "type": "line",
-          "utc": true,
-          "title": {
-            "y": "15px",
-            "text": "The average amount of CPU time spent in system mode",
-            "background-color": "none",
-            "font-color": "#05636c",
-            "font-size": "24px",
-            "height": "25px",
-            "adjust-layout": true
+        graphset: [
+          {
+            type: 'line',
+            utc: true,
+            title: {
+              y: '15px',
+              text: 'The average amount of CPU time spent in system mode',
+              'background-color': 'none',
+              'font-color': '#05636c',
+              'font-size': '24px',
+              height: '25px',
+              'adjust-layout': true,
+            },
+            plotarea: {
+              'margin-top': '10%',
+              'margin-right': 'dynamic',
+              'margin-bottom': 'dynamic',
+              'margin-left': 'dynamic',
+              'adjust-layout': true,
+              width: '100%',
+              height: '100%',
+            },
+            plot: {
+              animation: {
+                effect: 'ANIMATION_SLIDE_LEFT',
+                width: '100%',
+                height: '100%',
+              },
+            },
+            labels: this.labelFormat(),
+            'scale-x': {
+              label: {
+                'font-size': '14px',
+                'font-weight': 'normal',
+                'offset-x': '10%',
+                'font-angle': 360,
+              },
+              item: {
+                'text-align': 'center',
+                'font-color': '#05636c',
+              },
+              zooming: 1,
+              labels: this.dateFormat(),
+              'max-items': 6,
+              'min-items': 4,
+              'items-overlap': true,
+              guide: {
+                'line-width': '0px',
+              },
+              tick: {
+                'line-width': '2px',
+              },
+            },
+            'crosshair-x': {
+              'line-color': '#fff',
+              'line-width': 1,
+              'plot-label': {
+                visible: false,
+              },
+            },
+            'scale-y': {
+              item: {
+                'font-color': '#05636c',
+                'font-weight': 'normal',
+              },
+              label: {
+                text: 'CPU Range Data',
+                'font-size': '14px',
+              },
+              guide: {
+                'line-width': '0px',
+                alpha: 0.2,
+                'line-style': 'dashed',
+              },
+            },
+            plot: {
+              'line-width': 2,
+              marker: {
+                visible: false,
+              },
+              tooltip: {
+                'font-family': 'Roboto',
+                'font-size': '15px',
+                text: `%t value is %v`,
+                'text-align': 'left',
+                'border-radius': 5,
+                padding: 10,
+              },
+            },
+            series: this.dataFormat(),
           },
-          "plotarea": {
-            "margin-top": "10%",
-            "margin-right": "dynamic",
-            "margin-bottom": "dynamic",
-            "margin-left": "dynamic",
-            "adjust-layout": true,
-            'width':'100%',
-            'height': '100%'
-          },
-          "plot": {
-            "animation": {
-                "effect": "ANIMATION_SLIDE_LEFT",
-                'width':'100%',
-                'height': '100%'
-            },
-        },
-          "labels": this.labelFormat(),
-          "scale-x": {
-            "label": {
-              "font-size": "14px",
-              "font-weight": "normal",
-              "offset-x": "10%",
-              "font-angle": 360,
-            },
-            "item": {
-              "text-align": "center",
-              "font-color": "#05636c"
-            },
-            "zooming": 1,
-            labels: this.dateFormat(),
-            "max-items": 6,
-            "min-items": 4,
-            "items-overlap": true,
-            "guide": {
-              "line-width": "0px"
-            },
-            "tick": {
-              "line-width": "2px"
-            },
-          },
-          "crosshair-x": {
-            "line-color": "#fff",
-            "line-width": 1,
-            "plot-label": {
-              "visible": false
-            }
-          },
-          "scale-y": {
-            "item": {
-              "font-color": "#05636c",
-              "font-weight": "normal"
-            },
-            "label": {
-              "text": "CPU Range Data",
-              "font-size": "14px"
-            },
-            "guide": {
-              "line-width": "0px",
-              "alpha": 0.2,
-              "line-style": "dashed"
-            }
-          },
-          "plot": {
-            "line-width": 2,
-            "marker": {
-              "visible": false
-            },
-            "tooltip": {
-              "font-family": "Roboto",
-              "font-size": "15px",
-              "text": `%t value is %v`,
-              "text-align": "left",
-              "border-radius": 5,
-              "padding": 10
-            }
-          },
-          "series": this.dataFormat()
-        }]
-      }
+        ],
+      },
     };
   }
-
 
   dataFormat = () => {
     let outerContainer = [];
@@ -117,7 +112,17 @@ class QueryCpuRangeChart extends Component {
     let valueObj = {};
     let values = [];
     let timeValue;
-    let lineColor = ["#FF9AA2", "#FFB7B2", "#FFDAC1", "#E2F0CB", "#B5EAD7", "#C7CEEA", "#9ED2F6", "#9DDCE0", "#ADD4FF"];
+    let lineColor = [
+      '#FF9AA2',
+      '#FFB7B2',
+      '#FFDAC1',
+      '#E2F0CB',
+      '#B5EAD7',
+      '#C7CEEA',
+      '#9ED2F6',
+      '#9DDCE0',
+      '#ADD4FF',
+    ];
 
     for (let i = 0; i < dataLength; i++) {
       timeValue = this.props.cpuRangeChart[i][1];
@@ -126,24 +131,22 @@ class QueryCpuRangeChart extends Component {
         values.push(Number(timeValue[j][1]));
       }
 
-      valueObj = {
-        "values": values,
-        "line-color": lineColor[i % lineColor.length],
-        "aspect": "spline",
-        "background-color": lineColor[i % lineColor.length],
-        "alpha-area": ".5",
-        "font-family": "Roboto",
-        "font-size": "14px",
-        "text": `${this.props.cpuRangeChart[i][0]}`
-      },
-
-      outerContainer.push(valueObj);
+      (valueObj = {
+        values: values,
+        'line-color': lineColor[i % lineColor.length],
+        aspect: 'spline',
+        'background-color': lineColor[i % lineColor.length],
+        'alpha-area': '.5',
+        'font-family': 'Roboto',
+        'font-size': '14px',
+        text: `${this.props.cpuRangeChart[i][0]}`,
+      }),
+        outerContainer.push(valueObj);
       valueObj = {};
       values = [];
     }
-    console.log('refactor', outerContainer);
     return outerContainer;
-  }
+  };
 
   dateFormat = () => {
     let dateArr = [];
@@ -157,101 +160,121 @@ class QueryCpuRangeChart extends Component {
       for (let j = 0; j < timeValue.length; j++) {
         let time = `${new Date(Number(timeValue[j][0]) * 1000).toUTCString()}`;
         let timeArr = time.split(' ');
-        //["Mon,", "13", "Dec", "2021", "19:20:43", "GMT"]
         let timePiece = timeArr[4].split(':');
-        if (timePiece[0] === "01" || timePiece[0] === "02" || timePiece[0] === "03" || timePiece[0] === "04" || timePiece[0] === "05" || timePiece[0] === "06" || timePiece[0] === "07" || timePiece[0] === "08" || timePiece[0] === "09" || timePiece[0] === "10" || timePiece[0] === "11") {
-          formatTime = `${timeArr[0]} ${timeArr[1]} ${timeArr[2]}\n${timeArr[4]} AM`
-        } else if (timePiece[0] === "00" ) {
+        if (
+          timePiece[0] === '01' ||
+          timePiece[0] === '02' ||
+          timePiece[0] === '03' ||
+          timePiece[0] === '04' ||
+          timePiece[0] === '05' ||
+          timePiece[0] === '06' ||
+          timePiece[0] === '07' ||
+          timePiece[0] === '08' ||
+          timePiece[0] === '09' ||
+          timePiece[0] === '10' ||
+          timePiece[0] === '11'
+        ) {
+          formatTime = `${timeArr[0]} ${timeArr[1]} ${timeArr[2]}\n${timeArr[4]} AM`;
+        } else if (timePiece[0] === '00') {
           timePiece[0] = '12';
           let afterJoin = timePiece.join(':');
-          formatTime = `${timeArr[0]} ${timeArr[1]} ${timeArr[2]}\n${afterJoin} AM`
-        } else if (timePiece[0] === "12" || timePiece[0] === "13" || timePiece[0] === "14" || timePiece[0] === "15" || timePiece[0] === "16" || timePiece[0] === "17" || timePiece[0] === "18" || timePiece[0] === "19" || timePiece[0] === "20" || timePiece[0] === "21" || timePiece[0] === "22" || timePiece[0] === "23") {
+          formatTime = `${timeArr[0]} ${timeArr[1]} ${timeArr[2]}\n${afterJoin} AM`;
+        } else if (
+          timePiece[0] === '12' ||
+          timePiece[0] === '13' ||
+          timePiece[0] === '14' ||
+          timePiece[0] === '15' ||
+          timePiece[0] === '16' ||
+          timePiece[0] === '17' ||
+          timePiece[0] === '18' ||
+          timePiece[0] === '19' ||
+          timePiece[0] === '20' ||
+          timePiece[0] === '21' ||
+          timePiece[0] === '22' ||
+          timePiece[0] === '23'
+        ) {
           timePiece[0] = String(Number(timePiece[0]) - 12);
           let afterJoin = timePiece.join(':');
-          console.log('after',afterJoin)
-          formatTime = `${timeArr[0]} ${timeArr[1]} ${timeArr[2]}\n${afterJoin} PM`
+          formatTime = `${timeArr[0]} ${timeArr[1]} ${timeArr[2]}\n${afterJoin} PM`;
         }
         dateArr.push(formatTime);
       }
     }
     return dateArr;
-  }
+  };
 
   labelFormat = () => {
     const labelContainer = [];
     let labelObj = {};
     let dataLength = this.props.cpuRangeChart.length;
-    let lineColor = ["#FF9AA2", "#FFB7B2", "#FFDAC1", "#E2F0CB", "#B5EAD7", "#C7CEEA", "#9ED2F6", "#9DDCE0", "#ADD4FF"];
+    let lineColor = [
+      '#FF9AA2',
+      '#FFB7B2',
+      '#FFDAC1',
+      '#E2F0CB',
+      '#B5EAD7',
+      '#C7CEEA',
+      '#9ED2F6',
+      '#9DDCE0',
+      '#ADD4FF',
+    ];
 
-    // const gap = Math.floor(100 / dataLength);
     let start = 5;
     let secondStart = 15;
     let thirdStart = 25;
 
     for (let i = 0; i < dataLength; i++) {
-
       if (i <= 4) {
         labelObj = {
-          "text": `${this.props.cpuRangeChart[i][0]}: %plot-${i}-value`,
-          "default-value": "",
-          "color": lineColor[i % lineColor.length],
-          "x": `${start}%`,
-          "y": 50,
-          "width": 120,
-          "text-align": "left",
-          "bold": 0,
-          "font-size": "14px",
-          "font-weight": "bold",
-        }
+          text: `${this.props.cpuRangeChart[i][0]}: %plot-${i}-value`,
+          'default-value': '',
+          color: lineColor[i % lineColor.length],
+          x: `${start}%`,
+          y: 50,
+          width: 120,
+          'text-align': 'left',
+          bold: 0,
+          'font-size': '14px',
+          'font-weight': 'bold',
+        };
         start = start + 20;
       } else if (i <= 9) {
-          labelObj = {
-            "text": `${this.props.cpuRangeChart[i][0]}: %plot-${i}-value`,
-            "default-value": "",
-            "color": lineColor[i % lineColor.length],
-            "x": `${secondStart}%`,
-            "y": 65,
-            "width": 120,
-            "text-align": "left",
-            "bold": 0,
-            "font-size": "14px",
-            "font-weight": "bold",
-          }
-          secondStart = secondStart + 20;
-        } else if (i <= 14) {
-          labelObj = {
-            "text": `${this.props.cpuRangeChart[i][0]}: %plot-${i}-value`,
-            "default-value": "",
-            "color": lineColor[i % lineColor.length],
-            "x": `${thirdStart}%`,
-            "y": 80,
-            "width": 120,
-            "text-align": "left",
-            "bold": 0,
-            "font-size": "14px",
-            "font-weight": "bold",
-          }
-          thirdStart = thirdStart + 20;
-
-        }
-        labelContainer.push(labelObj);
+        labelObj = {
+          text: `${this.props.cpuRangeChart[i][0]}: %plot-${i}-value`,
+          'default-value': '',
+          color: lineColor[i % lineColor.length],
+          x: `${secondStart}%`,
+          y: 65,
+          width: 120,
+          'text-align': 'left',
+          bold: 0,
+          'font-size': '14px',
+          'font-weight': 'bold',
+        };
+        secondStart = secondStart + 20;
+      } else if (i <= 14) {
+        labelObj = {
+          text: `${this.props.cpuRangeChart[i][0]}: %plot-${i}-value`,
+          'default-value': '',
+          color: lineColor[i % lineColor.length],
+          x: `${thirdStart}%`,
+          y: 80,
+          width: 120,
+          'text-align': 'left',
+          bold: 0,
+          'font-size': '14px',
+          'font-weight': 'bold',
+        };
+        thirdStart = thirdStart + 20;
       }
-      return labelContainer;
-  }
-
+      labelContainer.push(labelObj);
+    }
+    return labelContainer;
+  };
 
   render() {
-    return (
-      <div>
-        <ZingChart id="querycpurangechart" data={this.state.config} />
-      </div>
-    );
+    return <ZingChart id="querycpurangechart" data={this.state.config} />;
   }
 }
 
-export default connect(
-  state => ({
-    cpuRangeChart: state.metricsReducer.cpuRangeChart,
-  }),
-  null
-)(QueryCpuRangeChart);
+export default QueryCpuRangeChart;
